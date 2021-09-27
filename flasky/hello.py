c95b826bdb30ed7880cb4c4fc17cbe300c54d553
Cansin Varol <cansinvarol@me.com>
2021-09-26 23:12:21 -0400

    fourth flask example with email validation

--- flasky/hello.py
+++ flasky/hello.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from flask_moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, Email, ValidationError
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Trident'
 bootstrap = Bootstrap(app)
@@ -12,7 +12,12 @@ moment = Moment(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
+    email = StringField('What is your UofT email address?', validators=[Required(), Email()])
     submit = SubmitField('Submit')
+
+    def validate_email(form,  field):
+        if "utoronto" not in field.data:
+            raise ValidationError("Please use your UofT email")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -22,8 +27,9 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
